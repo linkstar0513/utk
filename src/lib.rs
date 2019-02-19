d9f6b05a817e17da1761f8c087a3a7ea94305803
linkstar0513 <linkstar0513@gmail.com>
2019-02-19 12:16:23 +0800

    utk init

---
+++ src/lib.rs
@@ -0,0 +1,18 @@
+
+pub fn translate(){
+    println!("translate");
+}
+
+pub mod app;
+pub mod window;
+pub mod event;
+pub mod audio;
+pub mod video;
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
