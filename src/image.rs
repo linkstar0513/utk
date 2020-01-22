23776b243bb33757dd2250a65d1c2240cb6716e4
Haike <linkstar0513@gmail.com>
2020-01-22 20:25:45 +0800

    ImageBuffer Canvas

--- src/image.rs
+++ src/image.rs
@@ -2,17 +2,23 @@ pub enum PixelType{
     RGBA,
 }
 
+pub trait Canvas{
+    fn draw_line(&mut self, start: (usize, usize), end: (usize, usize));
+}
+
 pub struct ImageBuffer {
     width: usize,
     height: usize,
+    pixel_type: PixelType,
     buffer: Vec<u8>,
 }
 
 impl ImageBuffer {
-    pub fn new(width: usize, height: usize, imageType: PixelType ) -> Self{
+    pub fn new(width: usize, height: usize, pixel_type: PixelType ) -> Self{
         ImageBuffer {
             width,
             height,
+            pixel_type,
             buffer: vec![0; width * height * 4],
         }
     }
@@ -22,10 +28,27 @@ impl ImageBuffer {
         let buffer = &self.buffer;
         [buffer[start], buffer[start + 1], buffer[start + 2], buffer[start + 3]]
     }
+    pub fn set_pixel(&mut self, rth: usize, cth: usize, pixel: [u8;4]){
+        let start = ((rth - 1) * self.width + (cth - 1)) * 4;
+        let mut buffer = &mut self.buffer;
+        buffer[start + 0] = pixel[0];
+        buffer[start + 1] = pixel[1];
+        buffer[start + 2] = pixel[2];
+        buffer[start + 3] = pixel[3];
+    }
     pub fn get_pixel_enumerate_position(&self, rth: usize, cth: usize) -> [u8; 4] {
         let start = (rth * self.width + cth) * 4;
         let buffer = &self.buffer;
         [buffer[start], buffer[start + 1], buffer[start + 2], buffer[start + 3]]
+    }
+}
+
+impl Canvas for ImageBuffer {
+    fn draw_line(&mut self, start: (usize, usize), end: (usize, usize)){
+        for pos_x in start.0..end.0{
+            self.set_pixel(pos_x, start.1, [255, 255, 255, 255]);
+        }
+
     }
 }
 
